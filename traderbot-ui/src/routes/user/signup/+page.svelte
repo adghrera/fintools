cc6ad90dd71c0c981090ed5bdf0e3d2d52103148
Aman <adghrera@gmail.com>
2024-06-16 15:06:50 -0700

    added a test login page

---
+++ traderbot-ui/src/routes/user/signup/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	/** @type {import('./$types').PageData} */
+	import { Button } from '$lib/components/ui/button';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import type { PageData } from './$types.js';
+	import SettingsForm from './settings-form.svelte';
+
+	export let data: PageData;
+</script>
+
+<SettingsForm data={data.form} />
