182af283556b397a3d605c1187a12bc9291d68bd
Aman <adghrera@gmail.com>
2024-06-16 14:25:57 -0700

    adding shadcn components

--- traderbot-ui/src/routes/demo/[slug]/+page.svelte
+++ traderbot-ui/src/routes/demo/[slug]/+page.svelte
@@ -1,5 +1,9 @@
-<script>
+<script lang="ts">
 	/** @type {import('./$types').PageData} */
+	import { Button } from '$lib/components/ui/button';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+
 	export let data;
 </script>
 
@@ -9,7 +13,41 @@
 </svelte:head>
 
 <div class="text-column">
+	<Breadcrumb.Root>
+		<Breadcrumb.List>
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
+			</Breadcrumb.Item>
+			<Breadcrumb.Separator />
+			<Breadcrumb.Item>
+				<Breadcrumb.Page>Breadcrumb</Breadcrumb.Page>
+			</Breadcrumb.Item>
+		</Breadcrumb.List>
+	</Breadcrumb.Root>
+
 	<h1>This is just a slug page</h1>
 	<p>Some content here {data.post.title}</p>
 	<div>{@html data.post.content}</div>
+	<Button>Click me</Button>
+
+	<AlertDialog.Root>
+		<AlertDialog.Trigger>Open</AlertDialog.Trigger>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action>Continue</AlertDialog.Action>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
 </div>
